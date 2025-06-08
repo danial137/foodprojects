09a8b34e6d864104265b7779d8dac8fb5f013476
danial137 <danial79fakhrabadi@gmail.com>
2025-06-09 00:09:57 +0330

    Update layout and home components for food landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import Home from '@/components/Home/Home'
 const HomePage = () => {
   return (
-    <div>Home Page</div>
+    <div>
+      <Home />
+    </div>
   )
 }
 
 export default HomePage
+
+// local host 3000
