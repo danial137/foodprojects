fa223de9f95f3a076b6a98cfdece65c3766cafc1
danial137 <danial79fakhrabadi@gmail.com>
2025-06-28 17:47:30 +0330

    Refactor RestaurantCard component to accept props for title and image, and update rendering logic

--- components/Home/Restaurant/RestaurantCard.tsx
+++ components/Home/Restaurant/RestaurantCard.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
-
+import Image from 'next/image'
 
 type props = {
-    
+  title: string;
+  image: string;
+
 }
 
-const RestaurantCard = () => {
+const RestaurantCard = ({image,title}:props) => {
   return (
-    <div>RestaurantCard</div>
+    <div>
+      <Image src={image} alt="img" width={400} height={400} className="w-full h-[250px] object-cover" />
+      <h1 className="mt-4 w-fit dark:hover:text-amber-300 hover:text-blue-950 transition-all duration-200 cursor-pointer text-base sm:text-lg font-bold">{title}</h1>
+    </div>
   )
 }
 
