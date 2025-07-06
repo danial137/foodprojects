78e47d9722330e9142ab3437fd83f282eef402b3
danial137 <danial79fakhrabadi@gmail.com>
2025-07-06 17:14:04 +0330

    Enhance Category component header and layout for better presentation

--- components/Home/Category/Category.tsx
+++ components/Home/Category/Category.tsx
@@ -35,7 +35,14 @@ const Category = () => {
 
     return (
         <div className='pt-16 pb-16'>
-            <h1>popular categories</h1>
+            <h1 className='text-xl sm:text-2xl text-center font-extrabold'>popular categories by food</h1>
+            <div className='w-[80%] mx-auto mt-10'>
+                <div className='flex flex-wrap gap-4 justify-center'>
+                    {categories.map((categories, i) => {
+                        return <span key={i} className='px-6 py-3 rounded-full cursor-pointer hover:bg-emerald-600 hover:text-white transition-all duration-300 bg-gray-100 dark:bg-gray-800 dark:text-white'>{categories.name}</span>
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
